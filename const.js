8b7349e70007492992853ebce41ab5ea56705adf
yqwoshuai <358805791@qq.com>
2018-12-16 13:43:22 +0800

    提交后端代码

---
+++ const.js
@@ -0,0 +1,33 @@
+const resUrl = 'http://192.168.2.102:8081'
+const mp3FilePath = 'c:/Users/35880/Desktop/public/mp3'
+
+const category = [
+	'Biomedicine',
+	'BusinessandManagement',
+	'ComputerScience',
+	'EarthSciences',
+	'Economics',
+	'Engineering',
+	'Education',
+	'Environment',
+	'Geography',
+	'History',
+	'Laws',
+	'LifeSciences',
+	'Literature',
+	'SocialSciences',
+	'MaterialsScience',
+	'Mathematics',
+	'MedicineAndPublicHealth',
+	'Philosophy',
+	'Physics',
+	'PoliticalScienceAndInternationalRelations',
+	'Psychology',
+	'Statistics'
+]
+
+module.exports = {
+	resUrl,
+	category,
+	mp3FilePath
+}
